8582c3659ac6c7d64e811ac43296cb24ffe13a89
Robb Traister <robb.traister@gmail.com>
2019-11-13 22:21:38 -0500

    make navigation work for noscript+mobile

--- src/views/app/components/nav/index.tsx
+++ src/views/app/components/nav/index.tsx
@@ -1,29 +1,38 @@
 'use strict'
 
-import React, { Ref, createRef, useState, useEffect } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 
 import styles from './styles.scss'
 import { useUserContext } from '../../contexts/user'
 
-const Tab = ({ children, className, to }: { children: any; className?: string; to: string }) =>
-  <li className={`${styles.tab} ${className || ''}`.trim()}><NavLink to={to}>{children}</NavLink></li>
+const Tab = ({
+  children,
+  className,
+  to
+}: {
+  children: any
+  className?: string
+  to: string
+}) => (
+  <li className={`${styles.tab} ${className || ''}`.trim()}>
+    <NavLink to={to}>{children}</NavLink>
+  </li>
+)
 
 const Profile = () => {
   const user = useUserContext()
   const name = (user && user.name) || 'Profile'
 
-  return <Tab to='/profile' className={styles.profile}>{name}</Tab>
+  return (
+    <Tab to='/profile' className={styles.profile}>
+      {name}
+    </Tab>
+  )
 }
 
 const Nav = () => {
-  const hamburger: Ref<HTMLDivElement> = createRef()
-  const [open, setOpen] = useState(false)
-
-  const toggleOpen = (e) => {
-    hamburger && hamburger.current && (window.getComputedStyle(hamburger.current).display !== 'none') && e.preventDefault()
-    setOpen((open) => !open)
-  }
+  const [open, setOpen] = useState(null)
 
   useEffect(() => {
     if (open) {
@@ -33,28 +42,44 @@ const Nav = () => {
     }
   }, [open])
 
-  return <>
-    <div className={styles.nav}>
-      <div className={styles.container}>
-        <div className={`${styles.hamburger} ${open ? styles.open : ''}`.trim()} onClick={toggleOpen} ref={hamburger} />
-        <Link to='/' className={styles.logo} onClick={toggleOpen}>
-          <div className={styles.hello}>Hello</div>
-          <div className={styles.wallet}>Wallet</div>
-        </Link>
-        <div className={`${styles.menu} ${open ? styles.open : ''}`.trim()}>
-          <div className={styles.shade} />
-          <ul className={styles.tabs}>
-            <Tab to='/home'>Home</Tab>
-            <Tab to='/score'>Score</Tab>
-            <Tab to='/guidance'>Guidance</Tab>
-            <Tab to='/accounts'>Accounts</Tab>
-            <Tab to='/budget'>Budget</Tab>
-          </ul>
+  const toggleOpen = () => setOpen(open => !open)
+
+  return (
+    <>
+      <div className={styles.nav}>
+        <div
+          className={`${styles.container} ${
+            open ? styles.open : open === false ? styles.closed : ''
+          }`.trim()}
+        >
+          <NavLink to='/home' className={`${styles.logo} ${styles.desktop}`}>
+            <div className={styles.hello}>Hello</div>
+            <div className={styles.wallet}>Wallet</div>
+          </NavLink>
+          <a
+            href='#'
+            className={`${styles.logo} ${styles.mobile}`}
+            onClick={toggleOpen}
+          >
+            <div className={styles.hello}>Hello</div>
+            <div className={styles.wallet}>Wallet</div>
+          </a>
+          <a href='#' className={styles.hamburger} onClick={toggleOpen} />
+          <div className={styles.menu}>
+            <div className={styles.shade} />
+            <ul className={styles.tabs}>
+              <Tab to='/home'>Home</Tab>
+              <Tab to='/score'>Score</Tab>
+              <Tab to='/guidance'>Guidance</Tab>
+              <Tab to='/accounts'>Accounts</Tab>
+              <Tab to='/budget'>Budget</Tab>
+            </ul>
+          </div>
+          <Profile />
         </div>
-        <Profile />
       </div>
-    </div>
-  </>
+    </>
+  )
 }
 
 export default Nav
