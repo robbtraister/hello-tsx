ac5975b6d5001e115bd67110c95276fa47638471
Robb Traister <robb.traister@sciencelogic.com>
2019-12-05 10:03:29 -0500

    set document title in scene component

--- src/views/app/components/presentational/scene/index.tsx
+++ src/views/app/components/presentational/scene/index.tsx
@@ -1,19 +1,25 @@
 'use strict'
 
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Title from '../title'
 import Widget from '../widget'
 
-const Scene = ({ title, children }) => (
-  <>
-    <Widget>
-      <Title>{title}</Title>
-    </Widget>
-    {children}
-  </>
-)
+const Scene = ({ title, children }) => {
+  useEffect(() => {
+    document.title = title
+  }, [title])
+
+  return (
+    <>
+      <Widget>
+        <Title>{title}</Title>
+      </Widget>
+      {children}
+    </>
+  )
+}
 
 Scene.propTypes = {
   children: PropTypes.node,
